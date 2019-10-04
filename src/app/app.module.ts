fc09b43563d2594a33885bb3d3874e192d694d58
Carleton Cabarrus <carleton.cabarrus@onshoreoutsourcing.com>
2019-10-04 12:28:06 -0500

    added enviroment details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AuthService } from 'src/services/auth.service';
+import { ToastModule } from 'ng-uikit-pro-standard';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastModule.forRoot(),
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
