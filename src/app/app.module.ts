b8b14bffc72fabec108d75cedd381406dd1b4e68
Carleton Cabarrus <carleton.cabarrus@onshoreoutsourcing.com>
2019-10-04 12:34:22 -0500

    adding authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthService } from 'src/services/auth.service';
-import { ToastModule } from 'ng-uikit-pro-standard';
+import { environment } from 'src/environments/environment';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireModule } from '@angular/fire';
+
+const firebase={
+    apiKey: 'AIzaSyB4-c3qKMbhvlM5U7urjliu1XQMHmrSSZ8',
+    authDomain: 'bigquerytaxi-3e6d0.firebaseapp.com',
+    databaseURL: 'https://bigquerytaxi-3e6d0.firebaseio.com',
+    projectId: 'bigquerytaxi-3e6d0',
+    storageBucket: 'gs://bigquerytaxi-3e6d0.appspot.com'
+}
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +22,8 @@ import { ToastModule } from 'ng-uikit-pro-standard';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ToastModule.forRoot(),
+    AngularFireModule.initializeApp(firebase),
+    AngularFireAuthModule
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
--- src/enviroment.ts
+++ src/enviroment.ts
@@ -1,10 +1,3 @@
 export const environment = {
-    production: false,
-    firebase: {
-      apiKey: 'AIzaSyB4-c3qKMbhvlM5U7urjliu1XQMHmrSSZ8',
-      authDomain: 'bigquerytaxi-3e6d0.firebaseapp.com',
-      databaseURL: 'https://bigquerytaxi-3e6d0.firebaseio.com',
-      projectId: 'bigquerytaxi-3e6d0',
-      storageBucket: 'gs://bigquerytaxi-3e6d0.appspot.com'
-    }
+    production: false
   };
